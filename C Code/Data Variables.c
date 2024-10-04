0a3da43eb6bf2986644c9241555c074758a78446
yashjani1997 <yashjani445@gmail.com>
2024-10-04 15:54:40 +0530

    variablr update

--- C Code/Data Variables.c
+++ C Code/Data Variables.c
@@ -18,3 +18,25 @@ int main()
     scanf("%c", ,mycharacter)
 	return 0;
 }
+
+
+---------------------------------------------------------------------
+
+int main()
+{
+	  int mynum ;
+	float mypersentage = 55.63;
+	char mycharacter = 'm';
+//	 mynum = 88;
+	printf("Good Day \n");
+	printf("%d \n" ,mynum );
+	printf("%c \n" ,mycharacter);
+	printf("%f \n",mypersentage);
+	printf("%d \n",mynum);
+	printf("\nmynum=%3.6d", mynum);
+    printf("\nmypersentage=%3f", mypersentage);
+    printf("\nmycharacter=%2c \n", mycharacter);
+    scanf("%d", mynum);
+    scanf("%c", ,mycharacter)
+	return 0;
+}
